a36e744616571128b76445f643fa45b1ea090eec
Marlon <marlonhcooper2@gmail.com>
2025-05-05 00:46:02 -0400

    Start grid function

---
+++ script.js
@@ -0,0 +1,8 @@
+const gridContainer = document.querySelector("#container");
+const gridDiv = document.createElement("div");
+
+gridDiv.textContent = "testing this stuff"
+gridContainer.appendChild(gridDiv);
+function makeGrid(){
+
+};
