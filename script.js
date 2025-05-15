f1d5347693bd98f376042896800a179b6de825eb
Marlon <marlonhcooper2@gmail.com>
2025-05-15 01:16:24 -0400

    Make functional grid. Border issues exist

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 /** @type {HTMLElement | null} */
 const grid = document.querySelector(".container");
+const cellSize = 40;
 
 function makeGrid(row){
     const gridSize = 800;
-    const cellSize = gridSize / row;
-    grid.setAttribute("style", "width: 800 px");
-    grid.setAttribute("style", "height: 800 px");
+   
+    // grid.setAttribute("style", "width: 800 px");
+    // grid.setAttribute("style", "height: 800 px");
 
     
     for (let i = 0; i < row * row; i++){
         const cell = document.createElement("div");
         cell.setAttribute("class", "cell");
-        cell.setAttribute("style", "width; `${cellsize}px`");
-        cell.style.width = "40 px";
-        cell.style.height = "40 px";
-        cell.textContent = "testing this div"
+        cell.offsetWidth = cellSize;
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+        grid.style.width = `${row * cellSize}px`;
+        grid.style.width = `${row * cellSize}px`;
+        cell.textContent = "test"
         grid.appendChild(cell);
     }
 };
