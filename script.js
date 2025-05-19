1674119b2f686972fa8ff94acabb3a34b8c44c83
Marlon <marlonhcooper2@gmail.com>
2025-05-19 00:23:00 -0400

    Fix border issues and make presentable

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 /** @type {HTMLElement | null} */
 const grid = document.querySelector(".container");
-const cellSize = 40;
+
 
 function makeGrid(row){
-    const gridSize = 800;
-   
-    // grid.setAttribute("style", "width: 800 px");
-    // grid.setAttribute("style", "height: 800 px");
-
-    
+ 
     for (let i = 0; i < row * row; i++){
         const cell = document.createElement("div");
+        const cellSize = 25;
         cell.setAttribute("class", "cell");
-        cell.offsetWidth = cellSize;
         cell.style.width = `${cellSize}px`;
         cell.style.height = `${cellSize}px`;
         grid.style.width = `${row * cellSize}px`;
         grid.style.width = `${row * cellSize}px`;
-        cell.textContent = "test"
+        cell.textContent = ''
         grid.appendChild(cell);
     }
 };
-makeGrid(3);
+makeGrid(14);
